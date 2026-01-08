27046219ed3019f115eec7432d9653be5872c266
Timothy Alvarez <timothyalvarez097@gmail.com>
2026-01-09 00:00:45 +0800

    Move Express to Vercel API function

---
+++ api/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "FitTrack API is live 🚀"
+  });
+});
+
+module.exports = app;
